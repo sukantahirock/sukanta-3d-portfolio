476adb5d833217efab31d231ff0fed9696a1b5ae
sukantahirock <haridasnag01715511031@gmail.com>
2025-06-25 12:28:23 +0600

    Initial commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+export const Menu = () => {
+  return (
+    <div className="menu">
+      <div>Sukanta Nag Hirock</div>
+      <div className="menu__buttons">
+        <a className="menu__button" href="#home">
+          Home
+        </a>
+        <a className="menu__button" href="#skills">
+          Skills
+        </a>
+        <a className="menu__button" href="#projects">
+          Projects
+        </a>
+        <a className="menu__button" href="#contact">
+          Contact
+        </a>
+      </div>
+    </div>
+  );
+};
---
+++ src/config.js
@@ -0,0 +1,75 @@
+export const config = {
+  title: "Sukanta Nag Hirock",
+  sections: ["home", "skills", "projects", "contact"],
+  home: {
+    title: "Sukanta  Nag",
+    subtitle: "Hirock",
+  },
+  skills: [
+    {
+      name: "Python",
+      icon: "icons/python.png",
+      level: 95,
+    },
+    {
+      name: "JavaScript",
+      icon: "icons/javascript.png",
+      level: 80,
+    },
+    {
+      name: "Django",
+      icon: "icons/django.png",
+      level: 95,
+    },
+    {
+      name: "React",
+      icon: "icons/react-native.png",
+      level: 70,
+    },
+    {
+      name: "PyTorch",
+      icon: "icons/torch.png",
+      level: 90,
+    },
+    {
+      name: "English",
+      icon: "icons/usa.png",
+      level: 90,
+    },
+  ],
+  projects: [
+    {
+      name: "Inventory Managment System",
+      description: "An inventory managment system with Django with REST API",
+      image: "projects/project1.png",
+      link: "https://github.com/sukantahirock/Inventory-Managment-System",
+    },
+    {
+      name: "Marketing Dashboard",
+      description: "Campaign analysis using Looker Studio ,Pandas, Matplotlib",
+      image: "projects/project2.png",
+      link: "https://github.com/sukantahirock/data-visualization-looker-studio",
+    },
+    {
+      name: "Object Detection",
+      description: "Real time object detection with YOLOv11",
+      image: "projects/project3.png",
+      link: "https://github.com/sukantahirock",
+    },
+    {
+      name: "3D Portfolio",
+      description: "Personal a 3D portfolio",
+      image: "projects/project4.jpg",
+      link: "https://github.com/sukantahirock/sukanta-3d-portfolio",
+    },
+  ],
+  contact: {
+    name: "Sukanta Nag Hirock",
+    address: "Tangail, Bangladesh",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/sukanta-hirock-0bb15a34a/",
+      github: "https://github.com/sukantahirock",
+    },
+    mail: "haridasnag01715511031t@gmail.com",
+  },
+};
